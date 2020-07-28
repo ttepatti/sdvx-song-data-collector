e4b73bb46f63271f9fcae1ab6793229d7f497df4
ttepatti <tim@tepatti.com>
2020-07-27 22:58:09 -0400

    super basic scraping implemented

--- datacollector.py
+++ datacollector.py
@@ -2,6 +2,29 @@
 # Sound Voltex Song Data Collector
 # Scrapes Remiwiki for compiling information about Sound Voltex songs
 
-# https://remywiki.com/api.php?action=query&list=categorymembers&cmtitle=Category:SOUND_VOLTEX_Songs
+import requests
+import json
+
+#########
+# Get list of songs
+#########
+
+page = requests.get("https://remywiki.com/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:SOUND_VOLTEX_Songs")
+print(page.status_code)
+print(page.content)
+
+#json_data = json.loads(response_message)
+
+# parse json for song list
+
+#print(json_data['categorymembers'])
+
 # get cmcontinue value
-# run same string with cmcontinue value
+
+# run same string with cmcontinue value to get additional songs
+
+#########
+# Get song data
+#########
+
+# https://remywiki.com/api.php?action=query&format=json&prop=revisions&rvprop=content&rvslots=main&titles=%22Coconatsu%22%20wa%20yume%20no%20katachi
