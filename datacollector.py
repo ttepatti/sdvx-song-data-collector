26e300ea02f3e776a8abdc30e90a88e545d540bb
ttepatti <tim@tepatti.com>
2020-07-27 22:42:54 -0400

    added basic shell of a script

---
+++ datacollector.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Sound Voltex Song Data Collector
+# Scrapes Remiwiki for compiling information about Sound Voltex songs
+
+# https://remywiki.com/api.php?action=query&list=categorymembers&cmtitle=Category:SOUND_VOLTEX_Songs
+# get cmcontinue value
+# run same string with cmcontinue value
